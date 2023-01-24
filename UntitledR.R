823dc26c265dd5d99cc64249856791afd627bfa4
Is Ding <2431261252@qq.com>
2023-01-24 14:37:36 -0800

    updated question 1

--- UntitledR.R
+++ UntitledR.R
@@ -21,7 +21,28 @@ for (i in week20) {
 arithMeanGlobal_active_power <- mean(dfWeek20$Global_active_power)
 # geometric mean is e^(mean(ln(i)))
 geomMeanGlobal_active_power <- exp(mean(log(dfWeek20$Global_active_power)))
+medianGlobal_active_power <- median(dfWeek20$Global_active_power)
+# TODO mode skipped
+counter <- 
+
+minGlobal_active_power <- min(dfWeek20$Global_active_power)
+maxGlobal_active_power <- max(dfWeek20$Global_active_power)
+
+# B. Global_reactive_power
+arithMeanGlobal_reactive_power <- mean(dfWeek20$Global_reactive_power)
+geomMeanGlobal_reactive_power <- exp(mean(log(dfWeek20$Global_reactive_power)))
+medianGlobal_reactive_power <- median(dfWeek20$Global_reactive_power)
+# TODO mode skipped
+
+minGlobal_reactive_power <- min(dfWeek20$Global_reactive_power)
+maxGlobal_reactive_power <- max(dfWeek20$Global_reactive_power)
+
+#C. Voltage
+arithMeanVoltage <- mean(dfWeek20$Voltage)
+geomMeanVoltage <- exp(mean(log(dfWeek20$Voltage)))
+medianVoltage <- median(dfWeek20$Voltage)
+# TODO mode skipped
 
 class(df)                                       # tests the type of variable df
-dfDay20 <- df[df$Date == "20/1/2007", ]          # date in format "DD/MM/YYYY"
+dfDay20 <- df[df$Date == "20/1/2007", ]         # date in format "DD/MM/YYYY"
                                                 # need week 20, not day 20
