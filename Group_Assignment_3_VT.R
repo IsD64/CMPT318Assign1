597d126fc3842e4b4daaaf55b242c144021e671a
v4n6e3t <81235065+v4n6e3t@users.noreply.github.com>
2023-02-21 04:13:19 -0800

    Add files via upload

--- Group_Assignment_3_VT.R
+++ Group_Assignment_3_VT.R
@@ -1,5 +1,5 @@
+library(depmixS4)
 library(tidyverse)
-library(zoo)
 
 #setwd("C:/Users/24312/Documents/code/R") is Issac's path
 setwd("C:/Users/thund/Desktop/Cmpt 318") # is Vincent's path
@@ -16,3 +16,32 @@ for (i in 3:9) {
   scaled_data <- scaled_data %>% mutate(tempcol = tempdata)
   colnames(scaled_data)[i] <- colname
 }
+
+#Look for a pattern in consumption (Global active power)
+p <- scaled_data %>% filter(Date == "1/1/2007" & Time > "07:00:00" & Time < "11:00:00") %>% ggplot(mapping = aes(x = Time, y = Global_active_power))
+p + geom_point()
+
+p2 <- scaled_data %>% filter(Date == "8/1/2007" & Time > "07:00:00" & Time < "11:00:00") %>% ggplot(mapping = aes(x = Time, y = Global_active_power))
+p2 + geom_point()
+
+p3 <- scaled_data %>% filter(Date == "15/1/2007" & Time > "07:00:00" & Time < "11:00:00") %>% ggplot(mapping = aes(x = Time, y = Global_active_power))
+p3 + geom_point()
+
+#From the plots we can see that from 07:00:00 - 11:00:00 on Mondays the data often has a spike before going back down, with occasional smaller spikes after
+TimeWindowDF <- data.frame()
+
+#60*24 = 1440 minutes per day, *7 = 10080 minutes in a week
+for (i in 1:52) {
+  temp <- data.frame()
+  temp <- scaled_data[(1 + 10080*(i-1)):(10080*(i-1)+(1440)),]
+  temp <- temp %>% filter(Time >= "07:00:00" & Time <= "11:00:00") %>% dplyr::select(Date,Time,Global_active_power)
+  TimeWindowDF <- rbind(TimeWindowDF,temp)
+}
+
+#Question 2
+#12532 entries in the data set divided by 52 weeks = 241
+#We need to try multiple nstates to find the best fit, may not need to do all 13 possibilities if smart with it
+
+model1 <- depmix(response = Global_active_power ~ 1, data = TimeWindowDF, nstates = 10, ntimes = rep(241,52))
+fitmodel1 <- fit(model1)
+summary(fitmodel1)
