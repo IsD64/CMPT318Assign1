60e252f92e736f818011e5a09dad6b876552329a
Is Ding <2431261252@qq.com>
2023-03-23 23:10:19 -0700

    added testing for fitted models

--- Term_Project_ID2.R
+++ Term_Project_ID2.R
@@ -89,7 +89,7 @@ pcasum
 
 ## Part 2, Work In Progress
 
-dfresult <- selecteddata[, c(3, 6, 7)]
+dfresult <- selecteddata[, c(3, 6)]
 weeks_before_2009 <- 154 - 47
 obs_per_week <- 301
 obs_first_week <- 97
@@ -98,108 +98,102 @@ df_train <- dfresult[1: entries_before_2009, ]
 df_test <- setdiff(dfresult, df_train)
 
 model_4states <- depmix(response = list(Global_intensity ~ 1,
-                                        Global_active_power ~ 1,
-                                        Sub_metering_1 ~ 1),
+                                        Global_active_power ~ 1),
                         data = df_train,
                         nstates = 4,
                         ntimes = c(obs_first_week,
                                    rep(obs_per_week, weeks_before_2009 - 1)),
-                        family = list(gaussian(), gaussian(), multinomial()))
+                        family = list(gaussian(), gaussian()))
 fit_model_4states <- fit(model_4states)
 summary(fit_model_4states)
 log_lik_4states <- logLik(fit_model_4states)
 BIC_4states <- BIC(fit_model_4states)
 
 model_8states <- depmix(response = list(Global_intensity ~ 1,
-                                        Global_active_power ~ 1,
-                                        Sub_metering_1 ~ 1),
+                                        Global_active_power ~ 1),
                         data = df_train,
                         nstates = 8,
                         ntimes = c(obs_first_week,
                                    rep(obs_per_week, weeks_before_2009 - 1)),
-                        family = list(gaussian(), gaussian(), multinomial()))
+                        family = list(gaussian(), gaussian()))
 fit_model_8states <- fit(model_8states)
 summary(fit_model_8states)
 log_lik_8states <- logLik(fit_model_8states)
 BIC_8states <- BIC(fit_model_8states)
 
 model_12states <- depmix(response = list(Global_intensity ~ 1,
-                                         Global_active_power ~ 1,
-                                         Sub_metering_1 ~ 1),
+                                         Global_active_power ~ 1),
                          data = df_train,
                          nstates = 12,
                          ntimes = c(obs_first_week,
                                     rep(obs_per_week, weeks_before_2009 - 1)),
-                         family = list(gaussian(), gaussian(), multinomial()))
+                         family = list(gaussian(), gaussian()))
 fit_model_12states <- fit(model_12states)
 summary(fit_model_12states)
 log_lik_12states <- logLik(fit_model_12states)
 BIC_12states <- BIC(fit_model_12states)
 
 model_14states <- depmix(response = list(Global_intensity ~ 1,
-                                         Global_active_power ~ 1,
-                                         Sub_metering_1 ~ 1),
+                                         Global_active_power ~ 1),
                          data = df_train,
                          nstates = 14,
                          ntimes = c(obs_first_week,
                                     rep(obs_per_week, weeks_before_2009 - 1)),
-                         family = list(gaussian(), gaussian(), gaussian()))
+                         family = list(gaussian(), gaussian()))
 fit_model_14states <- fit(model_14states)
 summary(fit_model_14states)
 log_lik_14states <- logLik(fit_model_14states)
 BIC_14states <- BIC(fit_model_14states)
 
 model_16states <- depmix(response = list(Global_intensity ~ 1,
-                                         Global_active_power ~ 1,
-                                         Sub_metering_1 ~ 1),
+                                         Global_active_power ~ 1),
                          data = df_train,
                          nstates = 16,
                          ntimes = c(obs_first_week,
                                     rep(obs_per_week, weeks_before_2009 - 1)),
-                         family = list(gaussian(), gaussian(), gaussian()))
+                         family = list(gaussian(), gaussian()))
 fit_model_16states <- fit(model_16states)
 summary(fit_model_16states)
 log_lik_16states <- logLik(fit_model_16states)
 BIC_16states <- BIC(fit_model_16states)
 
 model_18states <- depmix(response = list(Global_intensity ~ 1,
-                                         Global_active_power ~ 1,
-                                         Sub_metering_1 ~ 1),
+                                         Global_active_power ~ 1),
                          data = df_train,
                          nstates = 18,
                          ntimes = c(obs_first_week,
                                     rep(obs_per_week, weeks_before_2009 - 1)),
-                         family = list(gaussian(), gaussian(), gaussian()))
+                         family = list(gaussian(), gaussian()))
 fit_model_18states <- fit(model_18states)
 summary(fit_model_18states)
 log_lik_18states <- logLik(fit_model_18states)
 BIC_18states <- BIC(fit_model_18states)
 
-model_20states <- depmix(response = list(Global_intensity ~ 1,
-                                         Global_active_power ~ 1,
-                                         Sub_metering_1 ~ 1),
-                         data = df_train,
-                         nstates = 20,
-                         ntimes = c(obs_first_week,
-                                    rep(obs_per_week, weeks_before_2009 - 1)),
-                         family = list(gaussian(), gaussian(), gaussian()))
-fit_model_20states <- fit(model_20states)
-summary(fit_model_20states)
-log_lik_20states <- logLik(fit_model_20states)
-BIC_20states <- BIC(fit_model_20states)
-
-model_24states <- depmix(response = list(Global_intensity ~ 1,
-                                         Global_active_power ~ 1,
-                                         Sub_metering_1 ~ 1),
-                         data = df_train,
-                         nstates = 24,
-                         ntimes = c(obs_first_week,
-                                    rep(obs_per_week, weeks_before_2009 - 1)),
-                         family = list(gaussian(), gaussian(), gaussian()))
-fit_model_24states <- fit(model_24states)
-summary(fit_model_24states)
-log_lik_24states <- logLik(fit_model_24states)
-BIC_24states <- BIC(fit_model_24states)
+# model_20states <- depmix(response = list(Global_intensity ~ 1,
+#                                          Global_active_power ~ 1,
+#                                          Sub_metering_1 ~ 1),
+#                          data = df_train,
+#                          nstates = 20,
+#                          ntimes = c(obs_first_week,
+#                                     rep(obs_per_week, weeks_before_2009 - 1)),
+#                          family = list(gaussian(), gaussian(), gaussian()))
+# fit_model_20states <- fit(model_20states)
+# summary(fit_model_20states)
+# log_lik_20states <- logLik(fit_model_20states)
+# BIC_20states <- BIC(fit_model_20states)
+# 
+# model_24states <- depmix(response = list(Global_intensity ~ 1,
+#                                          Global_active_power ~ 1,
+#                                          Sub_metering_1 ~ 1),
+#                          data = df_train,
+#                          nstates = 24,
+#                          ntimes = c(obs_first_week,
+#                                     rep(obs_per_week, weeks_before_2009 - 1)),
+#                          family = list(gaussian(), gaussian(), gaussian()))
+# fit_model_24states <- fit(model_24states)
+# summary(fit_model_24states)
+# log_lik_24states <- logLik(fit_model_24states)
+# BIC_24states <- BIC(fit_model_24states)
 
 model_performance <- data.frame(
     state_count = c(4, 8, 12, 14, 16, 18, 20, 24),
@@ -236,3 +230,48 @@ performance_plot <- p4 +
          y = "")
 
 performance_plot
+
+obs_last_week = 9935 - (301 * 33)
+
+test_14states <- depmix(response = list(Global_intensity ~ 1,
+                                        Global_active_power ~ 1),
+                        data = df_test,
+                        nstates = 14,
+                        ntimes = c(rep(obs_per_week,33),
+                                   obs_last_week),
+                        family = list(gaussian(), gaussian()))
+fit_test_14states <- setpars(test_14states, getpars(fit_model_14states))
+log_lik_test_14states <- forwardbackward(fit_test_14states)$logLike
+
+test_16states <- depmix(response = list(Global_intensity ~ 1,
+                                        Global_active_power ~ 1),
+                        data = df_test,
+                        nstates = 16,
+                        ntimes = c(rep(obs_per_week,33),
+                                   obs_last_week),
+                        family = list(gaussian(), gaussian()))
+fit_test_16states <- setpars(test_16states, getpars(fit_model_16states))
+log_lik_test_16states <- forwardbackward(fit_test_16states)$logLike
+
+test_18states <- depmix(response = list(Global_intensity ~ 1,
+                                        Global_active_power ~ 1),
+                        data = df_test,
+                        nstates = 18,
+                        ntimes = c(rep(obs_per_week,33),
+                                   obs_last_week),
+                        family = list(gaussian(), gaussian()))
+fit_test_18states <- setpars(test_18states, getpars(fit_model_18states))
+log_lik_test_18states <- forwardbackward(fit_test_18states)$logLike
+
+normalized_train_14states_loglik <- log_lik_14states / 32003
+normalized_train_16states_loglik <- log_lik_16states / 32003
+normalized_train_18states_loglik <- log_lik_18states / 32003
+normalized_test_14states_loglik <- log_lik_test_14states / 9935
+normalized_test_16states_loglik <- log_lik_test_16states / 9935
+normalized_test_18states_loglik <- log_lik_test_18states / 9935
+log_lik_diff_14states <- abs(normalized_test_14states_loglik -
+                             normalized_train_14states_loglik)
+log_lik_diff_16states <- abs(normalized_test_16states_loglik -
+                             normalized_train_16states_loglik)
+log_lik_diff_18states <- abs(normalized_test_18states_loglik -
+                             normalized_train_18states_loglik)
